bc07568092576ba637a11d36011d0e0a5c7be3f8
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2023-07-18 10:53:10 -0300

    update: project controller now has delete, update useCases

--- src/useCases/user/userController.ts
+++ src/useCases/user/userController.ts
@@ -1,8 +1,10 @@
 import { createUserController } from "./create/userCreate.Controller"
+import { deleteUser } from "./delete/userDelete.useCase"
+import { updateUser } from "./update/userUpdate.useCase"
 
 const userController = {
-  delete: "",
-  update: "",
+  delete: deleteUser,
+  update: updateUser,
   create: createUserController,
 }
 
